7838c309645628a726946be552c086cd10fa31d7
Fabian Monjes <fb.monjes@gmail.com>
2025-03-28 17:11:21 -0300

    valor a peso chileno.

--- backend/src/pokemon/pokemon.service.ts
+++ backend/src/pokemon/pokemon.service.ts
@@ -37,7 +37,7 @@ export class PokemonService {
         );
         }
     }
-    async getPokemonCardDetailsByNumber(name: string, number: string): Promise<{ name: string; number: string; imageUrl: string; price: number }> {
+    async getPokemonCardDetailsByNumber(name: string, number: string): Promise<{ nombre: string; numero: string; imagen: string; valor_actual: string; valor_minimo: string; valor_reversa: string; valor_maximo: string; }> {
         try {
         const apiKey = this.configService.get<string>('POKEMON_TCG_API_KEY');
         if (!apiKey) {
@@ -57,16 +57,52 @@ export class PokemonService {
         if (!card) {
             throw new HttpException('No se encontró la carta con ese número', HttpStatus.NOT_FOUND);
         }
-    
+        console.log(card.cardmarket);
+        console.log(card.cardmarket?.prices?.trendPrice);
+
         return {
-            name: `${card.name} ${card.number}`,
-            number: card.number,
-            imageUrl: card.images.large || card.images.small,
-            price: card.cardmarket?.prices?.trendPrice || 0
+            nombre: `${card.name} ${card.number}`,
+            numero: card.number,
+            imagen: card.images.large || card.images.small,
+            valor_actual: await this.convertPriceToChileanPesos(card.cardmarket?.prices?.trendPrice) || '',
+            valor_minimo: await this.convertPriceToChileanPesos(card.cardmarket?.prices?.lowPriceExPlus) || '',
+            valor_reversa : await this.convertPriceToChileanPesos(card.cardmarket?.prices?.reverseHoloTrend) || '',
+            valor_maximo : await this.convertPriceToChileanPesos(card.cardmarket?.prices?.avg30) || '',
         };
         } catch (error) {
         console.log(error);
         throw new HttpException('Error al obtener detalles de la carta', HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    async convertPriceToChileanPesos(price: number): Promise<string> {
+        try {
+            const apiKey = this.configService.get<string>('CURRENCY_API_KEY');
+            if (!apiKey) {
+                throw new HttpException('API Key no configurada', HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+    
+            const url = `https://open.er-api.com/v6/latest/USD`;
+            const headers = { 'X-Api-Key': apiKey };
+    
+            const response = await firstValueFrom(this.httpService.get(url, { headers }));
+    
+            if (!response.data || !response.data.rates || !response.data.rates.CLP) {
+                throw new HttpException('No se pudo obtener la tasa de cambio', HttpStatus.NOT_FOUND);
+            }
+    
+            const exchangeRate = response.data.rates.CLP;
+            
+            const priceChilean = Math.round(price * exchangeRate);
+
+            //colocar el precio en miles y con el signo $
+            const priceChileanFormatted = priceChilean.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+            const priceChileanWithSign = `$${priceChileanFormatted}`;
+
+            return priceChileanWithSign;
+        } catch (error) {
+            console.log(error);
+            throw new HttpException('Error al convertir el precio', HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }

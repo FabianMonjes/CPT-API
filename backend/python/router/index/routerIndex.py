fb1547d1991bf37f380fd7969b019e9b04f5353d
Dev Fabian Monjes <fb.monjes@gmail.com>
2025-04-27 23:47:40 -0400

    Update routerIndex.py

--- backend/python/router/index/routerIndex.py
+++ backend/python/router/index/routerIndex.py
@@ -85,3 +85,51 @@ async def buscar_pokemons(pokemon: str):
         raise http_error
     except Exception as e:
         return manejo_errores(e, "Error al buscar cartas de Pokémon", "buscar_pokemons")
+
+@index.get("/BusquedaPokemonDetalle/{nombre_carta}/{numero}")
+async def buscar_pokemon_detalle(nombre_carta: str, numero: str):
+    try:
+        print(f"Nombre de carta: {nombre_carta}, Número: {numero}")
+        load_dotenv()   
+        API_URL = os.getenv("POKEMON_TCG_API_URL")
+        API_KEY = os.getenv("POKEMON_TCG_API_KEY")
+        
+        if not nombre_carta:
+            raise HTTPException(status_code=400, detail="Nombre no proporcionado")
+        
+        if not numero:
+            raise HTTPException(status_code=400, detail="Nombre no proporcionado")
+        
+        if not API_KEY:
+            raise HTTPException(status_code=500, detail="API Key no configurada")
+        
+        url = f'{API_URL}?q=name:"{nombre_carta}" number:{numero}'
+        headers = {'X-Api-Key': API_KEY}
+        
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url, headers=headers)
+            response.raise_for_status()
+        
+        data = response.json()
+        
+        if not data or not data.get('data'):
+                raise HTTPException(status_code=404, detail="No se encontró la carta")
+        card = next((c for c in data['data'] if c['number'] == numero), None)
+        
+        if not card:
+            raise HTTPException(status_code=404, detail="No se encontró la carta con ese número")
+        
+        return {
+            "nombre": f"{card['name']} {card['number']}",
+            "numero": card['number'],
+            "imagen": card['images'].get('large') or card['images'].get('small', ''),
+            "valor_actual": card.get('cardmarket', {}).get('prices', {}).get('trendPrice', ''),
+            "valor_minimo": card.get('cardmarket', {}).get('prices', {}).get('lowPriceExPlus', ''),
+            "valor_reversa": card.get('cardmarket', {}).get('prices', {}).get('reverseHoloTrend', ''),
+            "valor_maximo": card.get('cardmarket', {}).get('prices', {}).get('avg30', '')
+        }
+    except HTTPException as http_error:
+        logger.error(f"HTTPException: {http_error.detail}")
+        raise http_error
+    except Exception as e:
+        return manejo_errores(e, "Error al buscar cartas de Pokémon", "buscar_pokemon_detalle")
